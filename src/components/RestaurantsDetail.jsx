55d8907e835f38aa8481f36ece635052855b8b7b
abadulrahaman <abadulrahamanrain@gmail.com>
2024-11-19 10:44:19 +0545

    Changes in open Category details when you click on its open only one

--- src/components/RestaurantsDetail.jsx
+++ src/components/RestaurantsDetail.jsx
@@ -1,45 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { IoFastFood } from "react-icons/io5";
-import { IoPin } from "react-icons/io5";
-import CategoryFoodDesc from "./CategoryFoodDesc";
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { IoFastFood } from 'react-icons/io5'
+import CategoryFoodDesc from './CategoryFoodDesc'
+import { MdLocationPin } from 'react-icons/md'
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
+
 //ShortName Cuisine Address1 VendorCoverImageName MinimumOrderAmount ServiceCharge VAT VendorLogoImageName
+
 const RestaurantDetail = () => {
+
   // "https://foodmandu.com/webapi/api/vendor/GetVendorDetail?VendorId=" +
   // "https://foodmandu.com/webapi/api/v2/Product/GetVendorProductsBySubCategoryV2?VendorId=" +
-  const [vendorData, setVendorData] = useState([]);
-  const [categoryData, setCategoryData] = useState([]);
-  const vendorUrlId = useParams();
-  const { id } = vendorUrlId;
+
+  const [vendorData, setVendorData] = useState([])
+  const [categoryData, setCategoryData] = useState([])
+  // const [openDetails, setOpenDetails] = useState(false)
+  const [categoryId, setCategoryId] = useState(null)
+  const vendorUrlId = useParams()
+  const { id } = vendorUrlId
   const getVendorDetail = async (id) => {
     try {
       const data = await fetch(
-        "https://foodmandu.com/webapi/api/vendor/GetVendorDetail?VendorId=" + id
-      );
-      const jsonData = await data.json();
-      setVendorData(jsonData[0]);
+        'https://foodmandu.com/webapi/api/vendor/GetVendorDetail?VendorId=' + id
+      )
+      const jsonData = await data.json()
+      setVendorData(jsonData[0])
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-  };
+  }
   const getCategoryData = async (id) => {
     try {
       const data = await fetch(
-        "https://foodmandu.com/webapi/api/v2/Product/GetVendorProductsBySubCategoryV2?VendorId=" +
+        'https://foodmandu.com/webapi/api/v2/Product/GetVendorProductsBySubCategoryV2?VendorId=' +
           id
-      );
-      const jsonData = await data.json();
-      setCategoryData(jsonData);
+      )
+      const jsonData = await data.json()
+      setCategoryData(jsonData)
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-  };
+  }
   useEffect(() => {
-    getVendorDetail(id);
-    getCategoryData(id);
-  }, []);
-  console.log(vendorData);
-  console.log(categoryData);
+    getVendorDetail(id)
+    getCategoryData(id)
+  }, [])
+
+  // const turnOpenDetails = () => {
+  //   setOpenDetails(!openDetails)
+  // }
+
+  const onlyOneCategoryOpen = (id) => {
+    setCategoryId(categoryId === id ? null : id)
+  }
+  
+  console.log(vendorData)
+  console.log(categoryData)
+
   return (
     <div>
       <div className="relative">
@@ -48,21 +65,21 @@ const RestaurantDetail = () => {
           alt=""
           className="h-[512px] w-full bg-cover"
         />
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-t from-black to-transparent opacity-70"></div>
-        <div className="container absolute bottom-14 right-0">
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-t from-black to-transparent opacity-70"></div> 
+        <div className="container absolute bottom-4 right-0">
           <div className="flex gap-4 ">
             <img
               src={vendorData.VendorLogoImageName}
               alt=""
-              className="h-32 w-22"
+              className="h-28 w-18"
             />
             <div className="text-xl text-white flex flex-col gap-2 justify-center">
-              <div className="flex gap-2">
+              <div className="flex items-center gap-2">
                 <IoFastFood />
                 <p className="text-white">{vendorData.ShortName}</p>
               </div>
-              <div className="flex gap-2">
-                <IoPin />
+              <div className="flex items-center gap-2">
+                <MdLocationPin />
                 <p>{vendorData.Address1}</p>
               </div>
             </div>
@@ -89,17 +106,32 @@ const RestaurantDetail = () => {
       <div className="flex justify-center items-center w-full py-2">
         <div className="w-[900px]">
           {categoryData.map((data) => (
-            <div key = {data.categoryId} className="w-full p-4">
-              <p className="bg-gray-200 p-4 text-xl">{data.category}</p>
-              {data.items.map((data) => (
-                <CategoryFoodDesc key = {data.productId} data = {data}/>
-              ))}
+            <div key={data.categoryId} className="w-full p-4">
+              <div 
+              className="flex justify-between items-center bg-gray-200 p-4 text-xl">
+                <p>{data.category}</p>
+
+                <div onClick={() => onlyOneCategoryOpen(data.categoryId)}>
+                  {categoryId === (data.categoryId) ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                </div>
+                </div>
+
+                {categoryId === (data.categoryId) &&(
+                  <div>
+                    {data.items.map((data) => (
+                      <CategoryFoodDesc key={data.productId} data={data} />
+                    ))}
+               </div>
+                )}
+
+
             </div>
           ))}
-        </div>  
+        </div>
       </div>
-
     </div>
   );
 };
+
+
 export default RestaurantDetail;
